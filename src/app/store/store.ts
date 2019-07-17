7f3e7ca3859b39fca2219bfda135c0f391c087ba
Toby Ho <airportyh@gmail.com>
2019-07-16 22:49:15 -0400

    Update by Drive By.

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -35,7 +35,7 @@ export class Store {
         const newValue = transform(this.subject$.value);
         this.subject$.next(newValue);
       } else {
-        const oldValue = _.get()
+        const oldValue = _.get(this.sub)
         const newValue = transform(this.subject$.value
         _.set(this.subject$.value, path, value);
         this.subject$.next(this.subject$.value);
