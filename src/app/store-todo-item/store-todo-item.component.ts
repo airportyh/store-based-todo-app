768d3803d5f9c6d31d05038d79c9cb8548c5d3cd
Toby Ho <airportyh@gmail.com>
2019-07-16 22:52:59 -0400

    Update by Drive By.

--- src/app/store-todo-item/store-todo-item.component.ts
+++ src/app/store-todo-item/store-todo-item.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class StoreTodoItemComponent implements OnInit {
 
   @Input() store: Store;
-  @Input() 
+  @Input() index
 
   constructor() { }
 
