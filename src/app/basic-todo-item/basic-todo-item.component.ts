14daa797d29435ac3f72f2ad07261875a72b5a95
Toby Ho <airportyh@gmail.com>
2019-07-16 21:47:36 -0400

    Update by Drive By.

--- src/app/basic-todo-item/basic-todo-item.component.ts
+++ src/app/basic-todo-item/basic-todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ITodo } from 'src/app/models/todo';
 
 @Component({
