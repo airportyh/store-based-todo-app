7caf8533f6371f1081515178e5b009d7a1ae3445
Toby Ho <airportyh@gmail.com>
2019-07-16 23:13:42 -0400

    Update by Drive By.

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -47,7 +47,7 @@ export class Store {
         this.subject$.value :
         get(path, this.subject$.value);
       const newArray = [...array, value];
-      const newState = set(path, newArray, this.sub)
+      const newState = set(path, newArray, this.subject$.)
       this.subject$.next(newArray);
     }
 }
