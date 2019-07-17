816b6f7dc8ffa135a44f0d202a1885fca0130cf3
Toby Ho <airportyh@gmail.com>
2019-07-16 21:47:18 -0400

    Update by Drive By.

--- src/app/basic-todo-item/basic-todo-item.component.ts
+++ src/app/basic-todo-item/basic-todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import { ITodo } from 'src/app/models/todo';
 import { EventEmitter } from '../../../node_modules/protractor';
 
