8f5a4dcc08cb75e2893271955b64aa4651d9f045
Toby Ho <airportyh@gmail.com>
2019-07-17 13:01:13 -0400

    Update by Drive By.

--- src/app/store/todo-store-2.ts
+++ src/app/store/todo-store-2.ts
@@ -1,5 +1,10 @@
 export class TodoStore {
   store: Store = new Store();
 
-  
+  const todo = {
+    name: this.newTodoName,
+    complete: false
+  };
+  this.store.push(["todos"], todo);
+  this.newTodoName = "";
 }
